5869b0c0d6252b1e884a059c5fb21747d87ee2e9
pef <pefbartimee.com@outlook.com>
2024-08-29 19:53:37 -0400

    Fix the interpretation error

--- hotfix.sh
+++ hotfix.sh
@@ -12,16 +12,24 @@ if command -v ip &> /dev/null
 then
     # Use ip route
     ip=$(ip route show | grep default | awk '{print $3}')
+    # Create DISPLAY and PULSE_SERVER variables in such a way that they are interpreted on run and not when this script is run
+    DISPLAY=$(ip route show | grep default | awk '{print $3}'):"0.0"
+    PULSE_SERVER="tcp:"$(ip route show | grep default | awk '{print $3}')
+
     echo "Using ip route with ip" $ip
 else
     # Otherwise, we use windows ipconfig
     ip=$(ipconfig.exe | grep -A 10 "vEthernet (WSL (Hyper-V firewall))" | grep "IPv4 Address" | sed -E 's/.*: ([0-9.]+)/\1/')
+
+    # Create DISPLAY and PULSE_SERVER variables in such a way that they are interpreted on run and not when this script is run
+    DISPLAY=$(ipconfig.exe | grep -A 10 "vEthernet (WSL (Hyper-V firewall))" | grep "IPv4 Address" | sed -E 's/.*: ([0-9.]+)/\1/'):"0.0"
+    PULSE_SERVER="tcp:"$(ipconfig.exe | grep -A 10 "vEthernet (WSL (Hyper-V firewall))" | grep "IPv4 Address" | sed -E 's/.*: ([0-9.]+)/\1/')
+
+
     echo "Using ipconfig with ip" $ip
 fi
 
-# Create DISPLAY and PULSE_SERVER variables
-DISPLAY="$ip:0.0"
-PULSE_SERVER="tcp:$ip"
+
 
 
 # Now, the user may be using bash, zsh, or fish.
